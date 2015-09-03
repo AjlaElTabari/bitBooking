ea6fe5f9a6f86aaa302c7351c83a735d6048d596
Edvin Mulabdic <edvin.mulabdic@bitcamp.ba>
2015-09-03 16:17:10 +0200

    fixed java script

---
+++ public/javascripts/frontpage.js
@@ -0,0 +1,11 @@
+/**
+ * Created by User on 9/3/2015.
+ */
+
+function PopUp(){
+    document.getElementById('ac-wrapper').style.display="none";
+}
+function logIn(){
+    document.getElementById('ac-wrapper').style.display="";
+}
+
