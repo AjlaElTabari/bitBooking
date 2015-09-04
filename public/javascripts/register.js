c2a6f99d788a2564a786c1a589e062492e0251d6
AjlaElTabari <ajla.eltabari@BitCamps-Mac-mini-65.local>
2015-09-04 11:41:03 +0200

    minor changes on javascript by Alen

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -4,13 +4,14 @@
 
 
 //function for email validation
+//returns error msg if password isnt written in a valid form
 function checkEmail() {
 
     var email = document.getElementById('email');
     var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
     if (!filter.test(email.value)) {
-        document.getElementById('email_error').innerHTML = "Please provide a valid email address!";
+        document.getElementById('email_error').innerHTML = "Please provide a valid email adress!";
         document.getElementById('submit').disable="true";
         return false;
     }else{
@@ -19,6 +20,9 @@ function checkEmail() {
         return true;
     }
 }
+
+//this function checks if passwors is more than 6 character long
+//returns error msg if password is less than 6 characters long
 function checkPasswordLength() {
 
     var pass1 = document.getElementById('password').value;
@@ -36,14 +40,15 @@ function checkPasswordLength() {
     }
 
 }
-//function that checks password match
+//function that checks if passwords match
+//returns error msg if passwords dont match
 function checkPasswordMatch() {
     var pass1 = document.getElementById("password").value;
     var pass2 = document.getElementById("password-retype").value;
 
 
     if (pass1 != pass2) {
-        document.getElementById('password2_error').innerHTML="Passwords do not match";
+        document.getElementById('password2_error').innerHTML="Passwords Do not match";
         document.getElementById('submit').disable="true";
         return false;
     }else {
@@ -57,6 +62,7 @@ function checkPasswordMatch() {
 
 
 //a function that checks if name and lastname contain only letters
+//returns error msg if name contains any other characters
 function checkName(){
     var letters = /^[A-Za-z]+$/;
     var name = document.getElementById("name");
@@ -67,13 +73,15 @@ function checkName(){
 
         return true;
     } else  {
-        document.getElementById('name_error').innerHTML="Name must contain alphabet characters only!";
+        document.getElementById('name_error').innerHTML="Name must have alphabet characters only!";
         document.getElementById('submit').disable="true";
         return false;
     }
 
 }
 
+//this functio checks if lastname contains only letters
+//returns error msg if lastname contains any other characters
 function checkLastname(){
     var letters = /^[A-Za-z]+$/;
     var lastname = document.getElementById("last-name");
@@ -84,7 +92,7 @@ function checkLastname(){
         document.getElementById('submit').disable="false";
         return true;
     } else  {
-        document.getElementById('lastname_error').innerHTML="Lastname must contain alphabet characters only!";
+        document.getElementById('lastname_error').innerHTML="Last Name must have alphabet characters only!";
         document.getElementById('submit').disable="true";
         return false;
     }
@@ -92,7 +100,8 @@ function checkLastname(){
 }
 
 
-//function that checks if name or lastname is longer than one character
+//function that checks if name is longer than one character
+//returns error msg if name contains only one or no characters
 function checkNameLength(){
     var name = document.getElementById("name").value;
 
@@ -108,7 +117,8 @@ function checkNameLength(){
     }
 }
 
-
+//function that checks if lastname is longer than one character
+//returns error msg if lastname contains only one character or no characters
 function checkLastnameLength(){
 
     var lastname = document.getElementById("last-name").value;
@@ -125,6 +135,7 @@ function checkLastnameLength(){
 }
 
 //a method that checks the length of phone number
+//returns error msg if number is more than 15 digits long
 function numberLength(){
     var number = document.getElementById("phone-number").value;
 
@@ -139,6 +150,8 @@ function numberLength(){
     }
 }
 
+//a function that checks if number contains only digits
+//returns error msg ig number contains other characters
 function isNumber(){
     var number = document.getElementById("phone-number").value;
 
@@ -154,6 +167,7 @@ function isNumber(){
 
 
 //a function that calls all the other functions
+//-not used
 function call() {
     checkEmail();
     checkPasswordMatch();
