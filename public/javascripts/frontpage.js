f355a8dbf95f861e44ab5f3589e94646e41fd54b
Edvin Mulabdic <edvin.mulabdic@bitcamp.ba>
2015-09-04 11:09:25 +0200

    Housekeeping

--- public/javascripts/frontpage.js
+++ public/javascripts/frontpage.js
@@ -2,10 +2,13 @@
  * Created by User on 9/3/2015.
  */
 
+/**function that deactivate pop up and activate front page**/
 function PopUp(){
     document.getElementById('ac-wrapper').style.display="none";
 }
+/**function that refreshes front page**/
 function logIn(){
-    document.getElementById('ac-wrapper').style.display="";
+    /**function that activate pop up**/
+    /**document.getElementById('ac-wrapper').style.display="";**/
+    window.location.href="/";
 }
-
