33b4bd20f77f21d82845da1e8b68cebbb4895dcc
Ajla El Tabari <ajla.eltabari@bitcamp.ba>
2015-09-11 07:14:55 +0200

    Added google maps

--- public/javascripts/coordinate.js
+++ public/javascripts/coordinate.js
@@ -1,92 +1,72 @@
-/**
- * Created by zeljko.miljevic on 10/09/15.
- */
 var geocoder = new google.maps.Geocoder();
 
-//this function is determining geocode position
 function geocodePosition(pos) {
-    geocoder.geocode({
-        latLng: pos
-    }, function(responses) {
-        if (responses && responses.length > 0) {
-            updateMarkerAddress(responses[0].formatted_address);
-        } else {
-            updateMarkerAddress('Cannot determine address at this location.');
-        }
-    });
+  geocoder.geocode({
+    latLng: pos
+  }, function(responses) {
+    if (responses && responses.length > 0) {
+      updateMarkerAddress(responses[0].formatted_address);
+    } else {
+      updateMarkerAddress('Cannot determine address at this location.');
+    }
+  });
 }
 
-//this function is updating marker status
 function updateMarkerStatus(str) {
-    document.getElementById('markerStatus').innerHTML = str;
+  document.getElementById('markerStatus').innerHTML = str;
 }
 
-//this function is updating marker position
 function updateMarkerPosition(latLng) {
-    document.getElementById('latitude').innerHTML = [
-        latLng.lat(),
-        latLng.lng()
-    ].join(', ');
-    document.getElementById('longitude').value = latLng.lat();
-    document.getElementById('latitude').value = latLng.lng();
+  document.getElementById('info').innerHTML = [
+    latLng.lat(),
+    latLng.lng()
+  ].join(', ');
+   document.getElementById('gLat').value = latLng.lat();
+   document.getElementById('gLng').value = latLng.lng();
 }
 
-//this function is updating coordinate
-function updateCoordinate(latLng) {
-    document.getElementById('latitude').value = latlng.lat;
-    document.getElementById('longitude').value = latlng.lng;
+function updateMarkerAddress(str) {
+  document.getElementById('address').innerHTML = str;
 }
 
-//this function is initializing the map
 function initialize() {
-    var latLng = new google.maps.LatLng(43.8534183, 18.3780840);
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: latLng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+  var latLng;
+  var x = document.getElementById('gLat').value;
+  var y = document.getElementById('gLng').value;
 
-    var marker = new google.maps.Marker({
-        position: latLng,
-        map: map,
-        animation:google.maps.Animation.BOUNCE,
-        title:'Click to zoom',
-        draggable: true
-    });
+  latLng = new google.maps.LatLng(x, y);
 
+  var map = new google.maps.Map(document.getElementById('mapCanvas'), {
+    zoom: 12,
+    center: latLng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+  var marker = new google.maps.Marker({
+    position: latLng,
+    title: 'Click here to zoom',
+    map: map,
+    draggable: true
+  });
 
-    google.maps.event.addListener(marker,'click',function() {
-        map.setZoom(16);
-        map.setCenter(marker.getPosition());
-        infowindow.open(map,marker);
-    });
+  // Update current position info.
+  updateMarkerPosition(latLng);
+  geocodePosition(latLng);
 
-    //create circle options
-    var circleOptions = {
-        fillColor: 'white',
-        map: map,
-        center: latLng,
-        radius: 1000
-    };
+  // Add dragging event listeners.
+  google.maps.event.addListener(marker, 'dragstart', function() {
+    updateMarkerAddress('Dragging...');
+  });
 
-    //create circle
-    myCircle = new google.maps.Circle(circleOptions);
+  google.maps.event.addListener(marker, 'drag', function() {
+    updateMarkerStatus('Dragging...');
+    updateMarkerPosition(marker.getPosition());
+  });
 
-    //when marker has completed the drag event
-    //recenter the circle on the marker.
-    google.maps.event.addListener(marker, 'dragend', function(){
-        myCircle.setCenter(this.position);
-    });
-
-    // Update current position info.
-    updateMarkerPosition(latLng);
-    geocodePosition(latLng);
-
-
-    google.maps.event.addListener(marker, 'drag', function() {
-        updateMarkerPosition(marker.getPosition());
-    });
+  google.maps.event.addListener(marker, 'dragend', function() {
+    updateMarkerStatus('Drag ended');
+    geocodePosition(marker.getPosition());
+  });
 }
 
-// Onload handler to start the app.
+// Onload handler to fire off the app.
 google.maps.event.addDomListener(window, 'load', initialize);
