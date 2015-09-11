9562f263d9ae6bdaa75a29608dced827f544bd8b
Edvin Mulabdic <edvin.mulabdic@bitcamp.ba>
2015-09-11 07:16:35 +0200

    Roles for Admin

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -28,10 +28,8 @@ public class Users extends Controller {
     private static Model.Finder<String, Feature> featureFinder = new Model.Finder<>(String.class, Feature.class);
 
 
-
-
     /*opening register form */
-    public Result register(){
+    public Result register() {
         return ok(register.render(userForm));
     }
 
@@ -40,48 +38,47 @@ public class Users extends Controller {
         Form<App_User> boundForm = userForm.bindFromRequest();
 
         //getting the values from the fields
-        String pass1 =boundForm.bindFromRequest().field("password").value();
+        String pass1 = boundForm.bindFromRequest().field("password").value();
         String pass2 = boundForm.bindFromRequest().field("passwordretype").value();
         String name = boundForm.bindFromRequest().field("firstname").value();
         String lastname = boundForm.bindFromRequest().field("lastname").value();
         String phone = boundForm.bindFromRequest().field("phoneNumber").value();
 
 
-
         //validation of the form
 
-        if(!pass1.equals(pass2)) {
+        if (!pass1.equals(pass2)) {
             flash("error", "Passwords don't match!");
             return ok(register.render(boundForm));
 
-        }else if(pass1.length() < 6 && pass2.length() < 6) {
+        } else if (pass1.length() < 6 && pass2.length() < 6) {
             flash("error", "Password must be at least 6 characters long!");
             return ok(register.render(boundForm));
 
-        }else if((!name.matches("^[a-zA-Z]+$/")) && (!lastname.matches("^[a-zA-Z]+$"))) {
+        } else if ((!name.matches("^[a-zA-Z]+$/")) && (!lastname.matches("^[a-zA-Z]+$"))) {
             flash("error", "Name and last name must contain letters only!");
             return ok(register.render(boundForm));
 
-        }else if(name.length() < 2 && lastname.length() < 2) {
+        } else if (name.length() < 2 && lastname.length() < 2) {
             flash("error", "Name and last name must be at least 2 letters long!");
             return ok(register.render(boundForm));
 
-        }else if(phone.length() > 15) {
+        } else if (phone.length() > 15) {
             flash("error", "Phone number can't be more than 15 digits long!");
             return ok(register.render(boundForm));
 
-        }else if(phone.matches("^[a-zA-Z]+$")) {
+        } else if (phone.matches("^[a-zA-Z]+$")) {
             flash("error", "Phone number must contain digits only!");
             return ok(register.render(boundForm));
 
-        }else{
+        } else {
             App_User user;
             try {
                 user = boundForm.get();
                 user.hashPass();
                 Ebean.save(user);
                 return redirect(routes.Application.index());
-            }catch (Exception e){
+            } catch (Exception e) {
                 flash("error", "Email already exist in our database, please try with another email!");
                 return ok(register.render(boundForm));
             }
@@ -104,12 +101,12 @@ public class Users extends Controller {
         App_User user = App_User.authenticate(email, password);
 
         if (user == null) {
-            flash("error","Incorrect email or password! Please try again!");
+            flash("error", "Incorrect email or password! Please try again!");
             return badRequest(list.render(hotels));
         } else {
             session().clear();
             session("email", email);
-            session("name",user.firstname);
+            session("name", user.firstname);
             session("id", user.userTypeId.toString());
 
             return ok(userProfilPage.render(user));
@@ -124,7 +121,8 @@ public class Users extends Controller {
     public Result logAdmin() {
         return ok(adminpanel.render());
     }
-    public Result logManager(){
+
+    public Result logManager() {
         return ok(managerpanel.render());
     }
 
@@ -132,38 +130,37 @@ public class Users extends Controller {
         session().clear();
         return ok(list.render(hotels));
     }
-    public Result showAdminHotels(){
+
+    public Result showAdminHotels() {
         List<Hotel> hotels = finder.all();
         return ok(adminhotels.render(hotels));
     }
 
     /*shows the list of users to admin*/
-    public Result showAdminUsers(){
+    public Result showAdminUsers() {
         List<App_User> users = userFinder.all();
         return ok(adminusers.render(users));
     }
+
     /*shows the list of features to admin*/
-    public Result showAdminFeatures(){
+    public Result showAdminFeatures() {
         List<Feature> features = featureFinder.all();
         return ok(adminfeatures.render(features));
 
     }
+
     /*shows the list of hotels to hotel manager*/
     public Result showManagerHotels() {
         List<Hotel> hotels = finder.all();
-        return ok (managerhotels.render(hotels));
+        return ok(managerhotels.render(hotels));
     }
 
     /*This method allows admin to delete user*/
-    public Result deleteUser(String email){
+    public Result deleteUser(String email) {
         App_User user = App_User.getUserByEmail(email);
         Ebean.delete(user);
         return redirect(routes.Users.showAdminUsers());
     }
-
-
-
-
 
 
     public Result updateUser(String email) {
@@ -177,21 +174,46 @@ public class Users extends Controller {
         String phone = boundForm.bindFromRequest().field("phoneNumber").value();
 
 
-        if(name != null) {
+        if (name != null) {
             user.firstname = name;
         }
-        if(lastname != null){
+        if (lastname != null) {
             user.lastname = lastname;
         }
-        if(!pass1.equals("") && pass1.equals(pass2)) {
+        if (!pass1.equals("") && pass1.equals(pass2)) {
             user.password = pass1;
             user.hashPass();
         }
-        if(phone != null) {
+        if (phone != null) {
             user.phoneNumber = phone;
         }
         Ebean.update(user);
 
         return redirect(routes.Users.editUser(user.email));
     }
+
+
+    public Result setRole(String email) {
+        Form<App_User> boundForm = userForm.bindFromRequest();
+
+        App_User user = App_User.getUserByEmail(email);
+        String userType = boundForm.bindFromRequest().field("usertype").value();
+
+
+
+        if (userType.equals("buyer")) {
+            user.userTypeId = 4;
+
+        } else if (userType.equals("seller")) {
+            user.userTypeId = 5;
+
+        } else if (userType.equals("hotelmanager")) {
+            user.userTypeId = 6;
+        }
+        Ebean.update(user);
+
+            return redirect(routes.Users.showAdminUsers());
+
+      }
+
 }
