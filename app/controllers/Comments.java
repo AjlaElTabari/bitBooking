110732ea77111069b24435e57df421d845b9a47d
Ajla El Tabari <ajla.eltabari@bitcamp.ba>
2015-09-18 00:48:07 +0200

    fixed issue with adding comments to the db

--- app/controllers/Comments.java
+++ app/controllers/Comments.java
@@ -1,10 +1,14 @@
 package controllers;
 
 import com.avaje.ebean.Ebean;
+import models.App_User;
 import models.Comment;
+import models.Hotel;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import java.util.logging.Logger;
 
 /**
  * Created by Zeljko Miljevic on 9/13/2015.
@@ -13,11 +17,18 @@ public class Comments extends Controller {
 
     private static final Form<Comment> commentForm = Form.form(Comment.class);
 
-    public Result insertComment() {
+    public Result insertComment(Integer hotelId) {
         Form<Comment> boundForm = commentForm.bindFromRequest();
 
 
         Comment comment = boundForm.get();
+
+        comment.hotel = Hotel.findHotelById(hotelId);
+        App_User user = App_User.getUserByEmail(request().cookies().get("email").value());
+
+        comment.user = user;
+
+
 
         Ebean.save(comment);
         return ok("Radi");
